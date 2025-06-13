906772994e3bde06a0a69da353b91ae4806d65d6
nicolas_duarte <nicolas.duarte@utec.edu.uy>
2025-06-13 11:00:46 -0300

    adds cors config class v2

--- src/main/java/org/utec/authservice/config/CorsGlobalConfig.java
+++ src/main/java/org/utec/authservice/config/CorsGlobalConfig.java
@@ -15,11 +15,11 @@ public class CorsGlobalConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        // ⚠️ En producción, reemplazá "*" por el dominio real del frontend
+        // En producción, debemos reemplazar "*" por el dominio real del frontend
         config.setAllowedOrigins(List.of("*"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
-        config.setAllowCredentials(false); // Cambiá a true si usás cookies/autenticación
+        config.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
